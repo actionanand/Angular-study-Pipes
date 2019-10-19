327dc60236afa6be5ba8c966909518fa4a22af99
actionanand <meetanandns@gmail.com>
2019-10-19 12:13:40 +0530

    Async pipe is tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,37 +12,49 @@ export class AppComponent {
       instanceType: 'medium',
       name: 'Production Server',
       status: 'critical',
-      started: new Date(13, 1, 2019)
+      started: new Date('Oct 19, 2019, 11:57:32 GMT+05.30')
     },
     {
       instanceType: 'large',
       name: 'User Database',
       status: 'stable',
-      started: new Date(21, 8, 2018)
+      started: new Date(2018,3,21,17,59,10)
     },
     {
       instanceType: 'small',
       name: 'Development Server',
       status: 'offline',
-      started: new Date(15, 1, 2017)
+      started: new Date(2018,7,17)
+    },
+    {
+      instanceType: 'small',
+      name: 'Old Server',
+      status: 'critical',
+      started: new Date(92,0,16)
     },
     {
       instanceType: 'small',
       name: 'Testing Environment Server',
       status: 'stable',
-      started: new Date(9, 3, 2017)
+      started: new Date(1551531600000)
     }
   ];
 
   filteredString ='';
   testServerCount:number = 1;
 
+  asyncStaus = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve('stable');
+    },2000);
+  });
+
   onAddNewServer(){
     this.servers.push({
       instanceType: 'small',
       name: 'Test Server '+ this.testServerCount,
       status: 'offline',
-      started: new Date(13, 11, 2013)
+      started: new Date()
     });
     this.testServerCount++;
   }
